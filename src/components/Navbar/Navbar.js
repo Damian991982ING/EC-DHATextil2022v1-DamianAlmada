4fcb46508e982ed98e193e578af05605db6ff10f
Damian991982ING <103780729+Damian991982ING@users.noreply.github.com>
2022-04-22 01:05:12 -0300

    Add files via upload

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Menu from "../svg/bars-solid.svg";
+import Close from "../svg/times-solid.svg";
+import CartWidget from "../CartWidget/CartWidget";
+
+
+import "./Navbar.css";
+
+
+const Nabvar=()=>{
+    return(
+        <header className="header navbar navbar-expand-lg navbar-light bg-light">
+            <div className="menu">
+                <img src={Menu} alt="/"/>
+            </div>
+            <div className="logo">
+                <h1><a href="/">DHATextil</a></h1>
+            </div>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Products</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Contact</a></li>
+                <li>
+                    <img src={Close} alt="" width="30"/>
+                </li>
+            </ul>
+            <div>
+               <CartWidget/>
+            </div>
+               
+            
+            
+            
+
+        </header>
+
+       
+
+    );
+};
+
+export default Nabvar;
